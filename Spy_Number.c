9b08a688efb94463e9542c96325c0f63ff87875a
saiyasaswi-685 <147792291+saiyasaswi-685@users.noreply.github.com>
2023-11-23 11:34:41 +0530

    #include<stdio.h> int main() {     int  n,s=0,r,p=1;     scanf("%d",&n);     while(n!=0)     {         r=n%10;         s=s+r;         p=p*r;         n=n/10;     }     if(p==s)     {       printf("Spy Number");     }     else{         printf("Not Spy Number");     } }

---
+++ Spy_Number.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int  n,s=0,r,p=1;
+    scanf("%d",&n);
+    while(n!=0)
+    {
+        r=n%10;
+        s=s+r;
+        p=p*r;
+        n=n/10;
+    }
+    if(p==s)
+    {
+      printf("Spy Number");  
+    }
+    else{
+        printf("Not Spy Number");
+    }
+}
