097637fe95b3814cba371f99106b76d46a101fa5
saiyasaswi-685 <147792291+saiyasaswi-685@users.noreply.github.com>
2023-11-26 21:19:20 +0530

    #include<stdio.h> int main() {     int n,last;     scanf("%d",&n);     last = n%10;     while(n!=0)     {         n=n/10;         if(n<10)         {             break;         }     }     printf("%d",last+n); }

--- Sum_of__First_and_Last_digits_.c
+++ Sum_of__First_and_Last_digits_.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 int main()
 {
-int n,i,last;
-
-scanf("%d",&n);
-last=n%10;
-while(n!=0){
-n=n/10;
-if(n<10)
-{
-break;
-}
-}
-printf("%d",last+n);
+    int n,last;
+    scanf("%d",&n);
+    last = n%10;
+    while(n!=0)
+    {
+        n=n/10;
+        if(n<10)
+        {
+            break;
+        }
+    }
+    printf("%d",last+n);
 }
