192c0850497221c4aca6f5d70701098b71c07eaa
saiyasaswi-685 <147792291+saiyasaswi-685@users.noreply.github.com>
2023-11-26 21:39:41 +0530

    #include<stdio.h> int main() {     int a,b,i,s,c;     scanf("%d%d",&a,&b);     for(i=a+1;i<b;i++)     {         s=i*i;         c=i*i*i;         printf("%d %d %d ",i,s,c);     }
    }

---
+++ N_-_S_-_C.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,i,s,c;
+    scanf("%d%d",&a,&b);
+    for(i=a+1;i<b;i++)
+    {
+        s=i*i;
+        c=i*i*i;
+        printf("%d %d %d
+",i,s,c);
+    }
+    
+    
+}
